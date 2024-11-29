2d786dfeff77ed5a59da87e2fcf7ede9661b7147
Dmitriy Zhivilov <xlukis44@gmail.com>
2024-11-29 10:34:30 +0300

    four commit

--- routes/auth.php
+++ routes/auth.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
-Route::post('logout', [AuthController::class, 'logout'])->middleware("auth:sanctum");
+Route::post('logout', [AuthController::class, 'logout']);
